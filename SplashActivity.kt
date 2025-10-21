4a7045e421f81b061c74dccc152a3fa4dd3533a4
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-21 10:26:02 +0100

    Create SplashActivity.kt

---
+++ SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.healthguardian.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.healthguardian.MainActivity
+import com.example.healthguardian.databinding.ActivitySplashBinding
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Launch the main activity after a 2.5s delay
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2500)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish() // close splash screen
+        }
+    }
+}
