fe0b06fa4b1ce7a15f8bd3d3abf2b0446409bba3
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-28 05:11:03 +0100

    Update HealthViewModel.kt

--- HealthViewModel.kt
+++ HealthViewModel.kt
@@ -3,9 +3,31 @@ package com.example.healthchecker.ui
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.example.healthchecker.data.HealthTipRepository
+import android.app.Application
+import com.example.healthchecker.data.HealthRecord
+import kotlinx.coroutines.launch
+import androidx.lifecycle.*
 
-class HealthViewModel(context: Context) : ViewModel() {
+class HealthViewModel(context: Context, application: Application) : AndroidViewModel(application) {
     private val repository = HealthTipRepository(context)
 
     fun getHealthTip(): String = repository.getRandomTip()
+}
+
+
+private val _records = MutableLiveData<List<HealthRecord>>()
+    val records: LiveData<List<HealthRecord>> = _records
+
+    fun loadRecords() {
+        viewModelScope.launch {
+            _records.value = dao.getAllRecords()
+        }
+    }
+
+    fun addRecord(record: HealthRecord) {
+        viewModelScope.launch {
+            dao.insertRecord(record)
+            loadRecords()
+        }
+    }
 }
