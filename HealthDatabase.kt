6d07975a2811b8f532f67ac61a980ae0adfa3748
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-05 04:59:29 +0100

    Update HealthDatabase.kt

--- HealthDatabase.kt
+++ HealthDatabase.kt
@@ -1,24 +1,26 @@
-package com.example.healthchecker
+package com.example.healthcheckerapp.data
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import android.content.Context
+import androidx.room.TypeConverters
+import com.example.healthcheckerapp.utils.Converters
 
-@Database(entities = [HealthData::class], version = 1)
+@Database(entities = [HealthData::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class HealthDatabase : RoomDatabase() {
     abstract fun healthDao(): HealthDao
 
     companion object {
-        @Volatile
-        private var INSTANCE: HealthDatabase? = null
+        @Volatile private var INSTANCE: HealthDatabase? = null
 
         fun getDatabase(context: Context): HealthDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     HealthDatabase::class.java,
-                    "health_database"
+                    "health_db"
                 ).build()
                 INSTANCE = instance
                 instance
