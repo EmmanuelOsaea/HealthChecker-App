bdd16d9c170dabdc29e2411a2430194d66671052
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-05 04:41:50 +0100

    Update HealthData.kt

--- HealthData.kt
+++ HealthData.kt
@@ -1,13 +1,22 @@
-package com.example.healthchecker
+
+
+
+
+package com.example.healthcheckerapp.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "health_data")
 data class HealthData(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val heartRate: Int,
+    val steps: Int,
+    val hydrationLevel: Int,
     val date: String,
     val bmi: Double,
     val steps: Int,
     val waterIntake: Int // in milliliters
-)
+    val recordedAt: Date = Date()
+     )
